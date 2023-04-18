b0b4f937e58e55d6994d53794054ca8341432a12
GordonOtieno <otienogordon95@gmail.com>
2023-04-18 21:06:53 +0300

    Update Task model, add association with status

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
-{
+{   
+    use SoftDeletes; // Add SoftDeletes trait
+
+    protected $dates = ['deleted_at']; // Add 'delete
     use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'description',
+        'due_date',
+        'status_id',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_tasks', 'task_id', 'user_id')
+            ->withPivot('due_date', 'start_time', 'end_time', 'remarks', 'status_id')
+            ->withTimestamps();
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
