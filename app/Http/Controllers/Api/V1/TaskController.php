20815504230e5ea6a6d24fea444c0153b5af5c20
GordonOtieno <otienogordon95@gmail.com>
2023-04-19 01:41:53 +0300

    update task controller add crud methods

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -2,26 +2,37 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\V1\TaskResource;
 
 class TaskController extends Controller
 {
     public function index(){
-        return response()->json('Hell from index');
+        return TaskResource::collection(Task::all());
+    }
+
+    public function show(Task $task){
+        return new TaskResource($task);
     }
 
     public function store(StoreTaskRequest $request){
         Task::create($request->validated());
-        return response()->json('Skills Created');
+        return response()->json('task Created successfully');
 
     }
 
     public function update(StoreTaskRequest $request, Task $task){
 
         $task->update($request->validated());
-        return response()->json('task updated');
+        return response()->json('task updated successfully');
+    }
+
+    public function destroy(Task $task){
+
+        $task->delete();
+        return response()->json('task deleted successfully');
     }
 }
