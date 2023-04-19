a884a9cb560ea493f163210c99a1de9f36b8db9b
GordonOtieno <otienogordon95@gmail.com>
2023-04-19 20:32:19 +0300

    Create and modify StatusController file

--- app/Http/Controllers/Api/V1/StatusController.php
+++ app/Http/Controllers/Api/V1/StatusController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Models\Status;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreStatusRequest;
+use App\Http\Resources\V1\StatusResource;
 
 class StatusController extends Controller
 {
-    //
+    public function index(){
+        return StatusResource::collection(Status::all());
+    }
+
+    public function store(StoreStatusRequest $request){
+        Status::create($request->validated());
+        return response()->json('Status Created successfully');
+
+    }
+
+    public function update(StoreStatusRequest $request, Status $status){
+
+        $status->update($request->validated());
+        return response()->json('status updated successfully');
+    }
+
+    public function destroy(Status $status){
+
+        $status->delete();
+        return response()->json('status deleted successfully');
+    }
 }
