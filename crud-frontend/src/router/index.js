8db8444161cd65a3dbef01a4f6b627f15e7ffdd3
GordonOtieno <otienogordon95@gmail.com>
2023-04-19 20:41:05 +0300

    Update router file

--- crud-frontend/src/router/index.js
+++ crud-frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TaskIndex from '../views/tasks/TaskIndex.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/tasks',
+      name: 'TaskIndex',
+      component: () => import('../views/tasks/TaskIndex.vue')
+    },
+    {
+      path: '/tasks/create',
+      name: 'TaskCreate',
+      component: () => import('../views/tasks/TaskCreate.vue')
+    },
+    {
+      path: '/tasks/:id/edit',
+      name: 'TaskEdit',
+      component: () => import('../views/tasks/TaskEdit.vue'),
+      props:true
     }
   ]
 })
