81b229865f9819f3aeae06d2c79faf71975498fe
GordonOtieno <otienogordon95@gmail.com>
2023-04-20 10:53:56 +0300

    Create authentication controller and add login and register method

--- crud-frontend/src/views/tasks/TaskCreate.vue
+++ crud-frontend/src/views/tasks/TaskCreate.vue
@@ -12,7 +12,7 @@ const form = reactive({
     name:"",
     description:"",
     due_date:"",
-    status_id:1,
+    status_id:'',
 })
 onMounted(()=>getStatuses())
 console.log(statuses)
@@ -42,11 +42,14 @@ console.log(statuses)
           <div v-if="errors.description">
           <span class="text-sm text-red-500">{{ errors.description[0] }}</span>
           </div> 
-        </div>
-          <div class="mb-6">
-          <label for="status_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User_id</label>
-          <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           </div>
+          <div class="mb-6">
+          <label for="status_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+          <select  v-model="form.status_id" id="status_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option v-for="status in statuses" :key="status.id" :value="status.id">{{ status.name }}</option>
+          </select>
+          </div>
+
         
         </div>
         <div class="mt-5">
